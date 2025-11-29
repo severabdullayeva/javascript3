55b1aaa58e45b617c7900f5a846f6255e9d53946
severabdullayeva <severabdullayeva>
2025-11-29 22:40:31 +0400

    new folder

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ console.log(a);
 console.log("stringin uzunlugu", a.length);
 
 // 2
-let b = Boolean(a.indexOf("e") + 1)
-console.log("'e' herfi var?", b)
+console.log("'e' herfi var?", Boolean(a.indexOf("e") + 1))
+// console.log("'e' herfi var?", Boolean(a.toLowerCase().indexOf("e") + 1))
 
 // 3
 console.log("'code' sozu ile baslayib?", a.startsWith("code"))
+// console.log("'code' sozu ile baslayib?", a.toLowerCase().startsWith("code"))
 
 // 4
 console.log("'code' sozu ile bitib?", a.endsWith("code"))
+// console.log("'code' sozu ile bitib?", a.toLowerCase().endsWith("code"))
 
 // 5
 console.log(a.replaceAll("t","####"))
+// console.log(a.toLowerCase().replaceAll("t","####"))
